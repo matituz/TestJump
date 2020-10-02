57fc3cd4262884efa3f325c24237b879f05c9eec
matituz <matituz@gmail.co>
2020-10-02 02:09:52 +0200

    News
    News

---
+++ Assets/Scripts/MoveBox.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBox : MonoBehaviour
+{
+    bool transformPlayer = false;
+    GameObject Player;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.name == "Player")
+        {
+            Player = collision.gameObject;
+            Player.transform.parent = transform;
+        }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        Player.transform.parent = null;
+    }
+}
---
+++ Assets/Scripts/ThirstPersonControler.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ThirstPersonControler : MonoBehaviour
+{
+    public GameObject PauseUi, CameraTarget;
+    private Rigidbody Rb;
+    Vector3 Move;
+    public Text DeathsText, TimeText, BestTime, PauseTimeText;
+    public float Speed = 20, RotationSpeed = 0.01f;
+    float Timer, MouseX;
+    bool JumpPressed = true, InColoder = true;
+    private void Start()
+    {
+        BestTime.text = PlayerPrefs.GetFloat("Time").ToString();
+        PauseUi.SetActive(false);
+        BestTime.text = PlayerPrefs.GetFloat("Time").ToString();
+        DeathsText.text = PlayerPrefs.GetInt("Deaths").ToString();
+        Rb = this.GetComponent<Rigidbody>();
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+    void Update()
+    {
+        Timer += Time.deltaTime;
+        TimeText.text = Timer.ToString("F2") + "s";
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        MouseX = MouseX + Input.GetAxis("Mouse X") * RotationSpeed;
+        this.GetComponent<Transform>().rotation = Quaternion.Euler(0, MouseX * 0.1f, 0);
+        Move = transform.right * x + transform.forward * z;
+        Rb.velocity = new Vector3(Move.x * Speed,0, Move.z * Speed);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (JumpPressed == true)
+            {
+                if (InColoder == true)
+                {
+                    Rb.AddForce(transform.up * 200 * Speed);
+                    JumpPressed = false;
+                    InColoder = false;
+                }
+            }
+        }
+        else
+        {
+            JumpPressed = true;
+            Rb.AddForce(transform.up * -15 * Speed);
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        InColoder = true;
+        if(collision.gameObject.name == "EndGame")
+        {
+            EndGame();
+        }
+        if (collision.gameObject.name == "Win")
+        {
+            WinGame();
+        }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        InColoder = false;
+    }
+    private void EndGame()
+    {
+        PlayerPrefs.SetInt("Deaths", PlayerPrefs.GetInt("Deaths") + 1);
+        SceneManager.LoadScene(0);
+    }
+    private void WinGame()
+    {
+        PauseUi.SetActive(true);
+        if(Timer < PlayerPrefs.GetFloat("Time"))
+        {
+            PlayerPrefs.SetFloat("Time", Timer);
+        }
+        if (PlayerPrefs.GetFloat("Time") == 0f)
+        {
+            PlayerPrefs.SetFloat("Time", Timer);
+        }
+        PauseTimeText.text = "Your time: " + Timer.ToString("F2") + "s";
+        BestTime.text = PlayerPrefs.GetFloat("Time").ToString();
+        Time.timeScale = 0;
+        Cursor.lockState = CursorLockMode.None;
+    }
+}
